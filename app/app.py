130120c6190c6b60c286b512c53256dae8b0e44e
Justin Farnsworth <farnswj1@tcnj.edu>
2022-07-15 15:03:40 -0400

    added x_port to proxy fix

--- app/app.py
+++ app/app.py
@@ -2,7 +2,14 @@ from flask import Flask, request, render_template
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_for=1,
+    x_proto=1,
+    x_host=1,
+    x_port=1,
+    x_prefix=1
+)
 
 
 @app.route('/', methods=['GET'])
